74d9bc72a0342bbc9e3d2622ffa2f75883a1f04b
risuney <honey197692@gmail.com>
2021-01-16 21:06:03 +0900

    add: Free size

--- qr-code.js
+++ qr-code.js
@@ -10,35 +10,15 @@ function hidden() {
 $('#make').on('click', function(){
   hidden();
   var input = $('#ip-qrcode').val();//inputのvalueを取得
+  var size = $('#size').val();
   var text = unescape(encodeURIComponent(input));//日本語対応
   if (input == '') {
     alert('文字を入力してください。\nPlease enter the characters.');
+  } else if (size > 0) {
+    $('#op-qrcode').qrcode({text: text, width: size, height: size});
+    canvas();
   } else {
     $('#op-qrcode').qrcode({text: text});
-    canvas();
-  }
-});
-
-$('#150').on('click', function(){
-  hidden();
-  var input = $('#ip-qrcode').val();//inputのvalueを取得
-  var text = unescape(encodeURIComponent(input));//日本語対応
-  if (input == '') {
-    alert('文字を入力してください。\nPlease enter the characters.');
-  } else {
-    $('#op-qrcode').qrcode({text:text, width:150, height:150});
-    canvas();
-  }
-});
-
-$('#250').on('click', function(){
-  hidden();
-  var input = $('#ip-qrcode').val();//inputのvalueを取得
-  var text = unescape(encodeURIComponent(input));//日本語対応
-  if (input == '') {
-    alert('文字を入力してください。\nPlease enter the characters.');
-  } else {
-    $('#op-qrcode').qrcode({text:text, width:250, height:250});
     canvas();
   }
 });
@@ -75,18 +55,6 @@ $('#1500').on('click', function(){
     alert('文字を入力してください。\nPlease enter the characters.');
   } else {
     $('#op-qrcode').qrcode({text:text, width:1500, height:1500});
-    canvas();
-  }
-});
-
-$('#2000').on('click', function(){
-  hidden();
-  var input = $('#ip-qrcode').val();//inputのvalueを取得
-  var text = unescape(encodeURIComponent(input));//日本語対応
-  if (input == '') {
-    alert('文字を入力してください。\nPlease enter the characters.');
-  } else {
-    $('#op-qrcode').qrcode({text:text, width:2000, height:2000});
     canvas();
   }
 });
